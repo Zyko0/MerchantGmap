0da4472bed7737c3d16ae22a7cd5c6aa91786d89
Bertrand Jung <bertrand.jung@epitech.eu>
2018-11-06 16:45:45 +0100

    fixed typo comment

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 		os.Exit(1)
 	}
 	companyName := os.Args[1]
-	b, err := ioutil.ReadFile("api.key") // regular file containing your raw clearbit api key
+	b, err := ioutil.ReadFile("api.key") // regular file containing your raw googlemaps api key
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
